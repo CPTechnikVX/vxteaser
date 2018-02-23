fea7c97ca1a42a4ea5e2806aeb7912d97b5de3d6
Petr Haslar <ph@campoint.net>
2018-02-23 15:14:15 +0000

    cleanup

--- src/utils/Processor.js
+++ src/utils/Processor.js
@@ -1,27 +1,5 @@
 import ElementFactory from './ElementFactory';
 
-if (typeof Object.assign !== 'function') {
-	Object.assign = function(target) {
-		'use strict';
-		if (target == null) {
-			throw new TypeError('Cannot convert undefined or null to object');
-		}
-
-		target = Object(target);
-		for (let index = 1; index < arguments.length; index++) {
-			const source = arguments[index];
-			if (source != null) {
-				for (const key in source) {
-					if (Object.prototype.hasOwnProperty.call(source, key)) {
-						target[key] = source[key];
-					}
-				}
-			}
-		}
-		return target;
-	};
-}
-
 /**
  * If the key is numeric
  * @param n
