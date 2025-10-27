c057523c6d3264d661c8afef7efd4549fee0178c
Petr Haslar <petr.haslar@campoint.net>
2025-10-27 10:05:16 +0000

    chore: update to react 19.2

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,12 +3,11 @@ const config = {
     stories:    ["../stories/index.stories.js"],
 
     addons:     [
-		"@storybook/addon-webpack5-compiler-swc",
-		"@storybook/addon-onboarding",
-		"@storybook/addon-essentials",
-		"@chromatic-com/storybook",
-		"@storybook/addon-interactions",
-	],
+        "@storybook/addon-webpack5-compiler-swc",
+        "@storybook/addon-onboarding",
+        "@chromatic-com/storybook",
+        "@storybook/addon-docs"
+    ],
 
     framework:  {
 		name:    "@storybook/react-webpack5",
--- .storybook/xconfig.js
+++ .storybook/xconfig.js
@@ -1,4 +1,4 @@
-import {configure} from '@storybook/react';
+import {configure} from '@storybook/react-webpack5';
 import './../http-root/lib/css/main.css';
 
 function loadStories() {
